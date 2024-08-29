b4136b61ae268d8d6f783b246446327d27c459ae
Silvia Pan <silvia.pan29@gmail.com>
2024-08-29 17:44:05 -0400

    process video locally with ffmpeg

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/process-video", (req, res) => {
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  if (!inputFilePath || !outputFilePath) {
+    res.send(400).send("Bad Request: Missing file path");
+  }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360")
+    .on("end", () => {
+      console.log("Processing finished successfully");
+      res.status(200).send("Processing finished successfully");
+    })
+    .on("error", (err: any) => {
+      console.log(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server runnng at http://localhost:${port}`);
